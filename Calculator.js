90d9b118e353bc32451af33dffc57eb973cd5345
Parth-Bhovad <parthbhovad710@gmail.com>
2024-04-12 20:29:09 +0530

    Added Files

---
+++ Calculator.js
@@ -0,0 +1,19 @@
+/*let inputBtn = document.querySelector(".input-btn");
+let numBtn = document.querySelectorAll(".no");
+
+let arr = numBtn;
+
+const exp = () => {
+  console.log("click");
+}
+
+inputBtn.addEventListener("click" , exp);
+
+const exp1 = () => {
+  console.log("btn click");
+}
+
+arr.addEventListener("click" , exp1);*/
+let input = document.querySelector(".input-area");
+
+input.innerText = "para";
